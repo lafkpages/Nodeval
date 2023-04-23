9231bf4661b4a205dbc8957dafcc23edaf691beb
lafkpages <mail@lafkpages.tech>
2023-04-23 11:10:42 +0200

    Fix cmdArgs's cmdStringToArgs

--- util/cmdArgs.js
+++ util/cmdArgs.js
@@ -17,7 +17,46 @@ function cmdArgsToString(args) {
 }
 
 function cmdStringToArgs(cmd, includeShC = true) {
-  return [...(includeShC ? ['sh', '-c'] : []), `'${escapeQuotes(cmd)}'`];
+  return includeShC
+    ? ['sh', '-c', `'${escapeQuotes(cmd)}'`]
+    : _cmdStringToArgs(cmd).map((chunk) =>
+        /\s/.test(chunk) ? `'${escapeQuotes(chunk)}'` : chunk
+      );
+}
+
+function _cmdStringToArgs(cmd) {
+  const result = [];
+  const log_matches = false;
+
+  const regex = /(([\w-/_~\.]+)|("(.*?)")|('(.*?)'))/g;
+  const groups = [2, 4, 6];
+  let match;
+
+  while ((match = regex.exec(cmd)) !== null) {
+    // This is necessary to avoid infinite loops
+    // with zero-width matches
+    if (match.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    // For this to work the regex groups need to
+    // be mutually exclusive
+    groups.forEach(function (group) {
+      if (match[group]) {
+        result.push(match[group]);
+      }
+    });
+
+    // show matches for debugging
+    log_matches &&
+      match.forEach(function (m, group) {
+        if (m) {
+          console.log(`Match '${m}' found in group: ${group}`);
+        }
+      });
+  }
+
+  return result;
 }
 
 module.exports = {
